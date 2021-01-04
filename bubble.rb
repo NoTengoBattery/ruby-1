322e55fd54c7f97adc6508e3376abab2e1aecad1
ixboy <ismaelixboy@gmail.com>
2021-01-05 00:13:26 +0300

    add if statement

--- bubble.rb
+++ bubble.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
 
 def bubble_sort_by(_input)
-  (_input.length - 1).times do |i|
+  (_input.length -1).times do |i|
     left = _input[i]
     right = _input[i + 1]
     testing = yield(left, right)
     testing = 0 <=> testing
+    if testing == 1
+_input[i + 1] = left
+_input[i] = right"
+end
   end
   _input
 end
