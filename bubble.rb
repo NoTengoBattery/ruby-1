0acc0be20ce72e9564554b0a34080afa37b7ee5f
Oever González <notengobattery@gmail.com>
2021-01-04 15:59:17 -0600

    bubble: add outer loop and some documentation

--- bubble.rb
+++ bubble.rb
@@ -1,15 +1,20 @@
 #!/usr/bin/env ruby
 
-def bubble_sort_by(_input)
-  (_input.length -1).times do |i|
-    left = _input[i]
-    right = _input[i + 1]
-    testing = yield(left, right)
-    testing = 0 <=> testing
-    if testing == 1
-_input[i + 1] = left
-_input[i] = right"
-end
+def bubble_sort_by(input)
+  # Calculate the number of outer iterations and start the outer loop
+  outer_iterations = input.length - 1
+  outer_iterations.times do |sorted_index|
+    # When we push a bubble, it is guaranteed that, at the end of that bubble, the element will be in the correct
+    # position. This means that we need to sort the remaining of the array, and safely forget about the just-sorted
+    # element and save some unneeded iterations
+    inner_iterations = outer_iterations - sorted_index
+    # Push the bubble, making the last element of the array to be in the correct position
+    inner_iterations.times do |index|
+      left_i = index
+      left = input[index]
+      right_i = index + 1
+      right = input[right_i]
+    end
   end
-  _input
+  input
 end
