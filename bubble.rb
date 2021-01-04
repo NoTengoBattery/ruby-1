f46e700eee032d876b548c1ef024fa1d6bf31cc5
Oever González <notengobattery@gmail.com>
2021-01-04 14:28:31 -0600

    bubble: implement the tests and part of the code

--- bubble.rb
+++ bubble.rb
@@ -1,3 +1,11 @@
 #!/usr/bin/env ruby
 
-def bubble_sort_by(input); end
+def bubble_sort_by(_input)
+  (_input.length - 1).times do |i|
+    left = _input[i]
+    right = _input[i + 1]
+    testing = yield(left, right)
+    testing = 0 <=> testing
+  end
+  _input
+end
--- spec/bubble_spec.rb
+++ spec/bubble_spec.rb
@@ -3,4 +3,20 @@
 require_relative '../bubble'
 
 RSpec.describe '#bubble_sort_by' do
+  it 'sorts words by length' do
+    unsorted = %w[aaaa a aaa aa]
+    sorted = bubble_sort_by(unsorted) do |left, right|
+      left.length - right.length
+    end
+    expected = %w[a aa aaa aaaa]
+    expect(sorted).to eq(expected)
+  end
+  it 'sorts numbers' do
+    unsorted = [1, 65, 23, 0]
+    sorted = bubble_sort_by(unsorted) do |left, right|
+      left - right
+    end
+    expected = [0, 1, 23, 65]
+    expect(sorted).to eq(expected)
+  end
 end
